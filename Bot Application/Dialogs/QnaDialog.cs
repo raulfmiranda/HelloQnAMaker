4937c3c8898b46765b843759d3d99a9e9a52d5f1
Raul Figueira <raulfmiranda@gmail.com>
2018-01-27 13:07:34 -0300

    Estrutura inicial

---
+++ Bot Application/Dialogs/QnaDialog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application.Dialogs
+{
+    [Serializable]
+    public class QnaDialog : QnAMakerDialog
+    {
+        public QnaDialog() : base(new QnAMakerService(new QnAMakerAttribute(            ConfigurationManager.AppSettings["QnaSubscriptionKey"],             ConfigurationManager.AppSettings["QnaKnowledgebaseId"],             "Não, encontrei sua resposta",0.5)))        {
+
+        }
+    }
+}
